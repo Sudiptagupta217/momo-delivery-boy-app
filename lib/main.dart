e51196cf4a55e1efcea45bbb7e63941b22c38ac6
Unika Compaq <sudiptagupta217@gmail.com>
2023-08-17 18:21:03 +0530

    create menu screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:it_momo_wala/ui/menu_screen.dart';
 import 'package:it_momo_wala/ui/splash_screen.dart';
 import 'package:it_momo_wala/utils/default_colors.dart';
 
@@ -22,7 +23,8 @@ class MyApp extends StatelessWidget {
 
       title: 'Flutter Demo',
       theme: ThemeData(),
-      home:  SplashScreen(),
+    //  home:  SplashScreen(),
+      home:  MenuScreen(),
     );
   }
 }
